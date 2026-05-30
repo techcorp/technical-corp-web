a889f7bc20c64b4f1bbc38d472edcb560a40724e
Technical Corp Official <hafizinsights@gmail.com>
2026-05-30 16:29:33 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter, Outfit } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
-import Script from "next/script";
 
 const inter = Inter({
   subsets: ["latin"],
